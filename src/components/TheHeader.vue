d1a881da86c1f59909ba4a1dfc6ffdaaf957532a
Son2k <theson1106@gmail.com>
2023-07-14 16:34:56 +0700

    first commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="flex pl-[18px] h-[52px]">
+    <div class="flex items-center">
+      <img
+        class="h-[30px] md:h-[50px]"
+        src="https://signal.org/assets/header/logo-f7ef605fe417d5520d38d546b3b774b4261c75220b9904da4d8b2ffc19a761ff.png"
+        alt="logo"
+      />
+    </div>
+
+    <TheHambuger />
+
+    <!-- <div class="flex-1 flex items-center">
+      <ul class="flex gap-4">
+        <li v-for="item in menu" :key="item.id">
+          <a :href="item.link">{{ item.name }}</a>
+        </li>
+      </ul>
+    </div> -->
+  </div>
+</template>
+
+<script setup>
+import TheHambuger from "./UI/TheHambuger.vue";
+
+const menu = [
+  {
+    id: 1,
+    name: "Get Signal",
+    link: "https://signal.org/vi/download/"
+  },
+  {
+    id: 2,
+    name: "Help",
+    link: "https://support.signal.org/"
+  },
+  {
+    id: 3,
+    name: "Blog",
+    link: "https://signal.org/blog/"
+  },
+  {
+    id: 4,
+    name: "Developers",
+    link: "https://signal.org/docs/"
+  },
+  {
+    id: 5,
+    name: "Careers",
+    link: "https://signal.org/workworkwork/"
+  },
+  {
+    id: 6,
+    name: "Donate",
+    link: "https://signal.org/vi/donate/"
+  }
+];
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/UI/TheHambuger.vue
@@ -0,0 +1,51 @@
+<template>
+  <a
+    @click="toggleMenu"
+    v-if="isMenuClose"
+    href="#"
+    class="w-[52px] flex items-center justify-center ml-auto relative"
+  >
+    <div
+      class="flex flex-col justify-center items-center w-4 h-7 cursor-pointer absolute transform translate-y-px"
+    >
+      <span class="w-full h-px bg-[#000] mb-1"></span>
+      <span class="w-full h-px bg-[#000] mb-1"></span>
+      <span class="w-full h-px bg-[#000] mb-1"></span>
+    </div>
+  </a>
+
+  <a
+    @click="toggleMenu"
+    v-if="!isMenuClose"
+    href="#"
+    class="w-[52px] flex items-center justify-center ml-auto relative"
+  >
+    <div
+      class="flex flex-col justify-center items-center w-4 h-7 cursor-pointer absolute transform translate-y-1"
+    >
+      <span class="w-full h-px bg-[#000] mb-1 transform rotate-45"></span>
+      <span
+        class="w-full h-px bg-[#000] mb-1 transform -rotate-45 -translate-y-[5px]"
+      ></span>
+    </div>
+  </a>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    let isMenuClose = ref(true);
+
+    console.log(isMenuClose, "isMenuClose");
+
+    const toggleMenu = () => {
+      isMenuClose.value = !isMenuClose.value;
+      console.log(isMenuClose.value, "isMenuClose");
+    };
+    return { isMenuClose, toggleMenu };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
